9e8518646f707cb1500a6baef8782e18dea050f5
Natalya Anatskaya <taff2011@yandex.ru>
2023-02-05 09:47:50 +0200

    верстка первого экрана. Кривой инпут через Cleave

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
-new Swiper('.slider')
+/*new Swiper('.slider')*/
+
+/*var cleave = new Cleave('.input-phone', {
+	phone: true,
+	phoneRegionCode: '{RU}',
+})*/
+
+/*var cleave = new Cleave('.input-phone', {
+	prefix: '+7 ',
+	delimiters: ['(', ')', '-'],
+	blocks: [2, 3, 3, 2, 2],
+	uppercase: true,
+})*/
