0f759f9c5336dd542ede6ed20ad4c0fc5aac2c31
Natalya Anatskaya <taff2011@yandex.ru>
2023-02-13 12:27:36 +0200

    верстка главной страницы с tab

--- js/main.js
+++ js/main.js
@@ -1,13 +1,33 @@
-/*new Swiper('.slider')*/
+const menuToggle = document.querySelectorAll('.menu__link')
+const menuTab = document.querySelectorAll('.tab')
+const menuCash = document.querySelectorAll('.menu__cash')
+console.log(menuCash)
 
-/*var cleave = new Cleave('.input-phone', {
-	phone: true,
-	phoneRegionCode: '{RU}',
-})*/
+for (let i = 0; i < menuToggle.length; i++) {
+	menuToggle[i].addEventListener('click', (event) => {
+		event.preventDefault()
+		/*удаляем класс фону*/
+		for (let b = 0; b < menuCash.length; b++) {
+			menuCash[b].classList.remove('menu__cash_sell')
+		}
 
-/*var cleave = new Cleave('.input-phone', {
-	prefix: '+7 ',
-	delimiters: ['(', ')', '-'],
-	blocks: [2, 3, 3, 2, 2],
-	uppercase: true,
-})*/
+		/*добавляем класс фону*/
+		menuCash[i].classList.add('menu__cash_sell')
+
+		/*удаляем класс ссылке*/
+		for (let e = 0; e < menuToggle.length; e++) {
+			menuToggle[e].classList.remove('menu__link_active')
+		}
+
+		/*добавляем класс ссылке*/
+		event.target.classList.add('menu__link_active')
+		/*удаляем класс контента*/
+		for (let a = 0; a < menuTab.length; a++) {
+			menuTab[a].classList.remove('tab__active')
+		}
+
+		/*добавляем класс контенту*/
+		menuTab[i].classList.add('tab__active')
+		console.log(menuTab[i])
+	})
+}
